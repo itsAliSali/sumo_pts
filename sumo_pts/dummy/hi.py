126cff63c6d92fc504ec05f784d8c457f2f5a56a
itsAliSali <ali.salimian@rwth-aachen.de>
2024-08-26 15:01:22 +0200

    removing dummy files

--- sumo_pts/dummy/hi.py
+++
@@ -1,8 +0,0 @@
-class Hi:
-
-    def __init__(self, dist):
-        self.dist = dist
-        
-    def foo(self, a):
-        print(f"{self.dist}, {a}")
-
--- sumo_pts/scenario/trips.py
+++
@@ -1,7 +0,0 @@
-def abs1(p):
-    print(f"abs: {p}")
-    
-def abs2(p):
-    print(f"abs 2222: {p}")
-    
-    
